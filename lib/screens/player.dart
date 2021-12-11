3e958d2eea510803f7859f33c32bfef2f860535e
scintillavoy <scintillavoy@gmail.com>
2021-12-11 22:22:35 +0900

    Add player buttons

---
+++ lib/screens/player.dart
@@ -0,0 +1,35 @@
+import 'package:discoid/screens/player_buttons.dart';
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+
+class Player extends StatefulWidget {
+  const Player({Key? key}) : super(key: key);
+
+  @override
+  _PlayerState createState() => _PlayerState();
+}
+
+class _PlayerState extends State<Player> {
+  late AudioPlayer _audioPlayer;
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer = AudioPlayer();
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: PlayerButtons(_audioPlayer),
+      ),
+    );
+  }
+}
---
+++ lib/screens/player_buttons.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+
+class PlayerButtons extends StatelessWidget {
+  const PlayerButtons(this._audioPlayer, {Key? key}) : super(key: key);
+
+  final AudioPlayer _audioPlayer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        StreamBuilder<bool>(
+          stream: _audioPlayer.shuffleModeEnabledStream,
+          builder: (context, snapshot) {
+            return _shuffleButton(context, snapshot.data ?? false);
+          },
+        ),
+        StreamBuilder<SequenceState?>(
+          stream: _audioPlayer.sequenceStateStream,
+          builder: (_, __) {
+            return _previousButton();
+          },
+        ),
+        StreamBuilder<PlayerState>(
+          stream: _audioPlayer.playerStateStream,
+          builder: (_, snapshot) {
+            final playerState = snapshot.data;
+            return _playPauseButton(playerState);
+          },
+        ),
+        StreamBuilder<SequenceState?>(
+          stream: _audioPlayer.sequenceStateStream,
+          builder: (_, __) {
+            return _nextButton();
+          },
+        ),
+        StreamBuilder<LoopMode>(
+          stream: _audioPlayer.loopModeStream,
+          builder: (context, snapshot) {
+            return _repeatButton(context, snapshot.data ?? LoopMode.off);
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _shuffleButton(BuildContext context, bool isEnabled) {
+    return IconButton(
+      icon: isEnabled
+          ? Icon(Icons.shuffle, color: Theme.of(context).colorScheme.secondary)
+          : const Icon(Icons.shuffle),
+      onPressed: () async {
+        final enable = !isEnabled;
+        if (enable) {
+          await _audioPlayer.shuffle();
+        }
+        await _audioPlayer.setShuffleModeEnabled(enable);
+      },
+    );
+  }
+
+  Widget _previousButton() {
+    return IconButton(
+      icon: const Icon(Icons.skip_previous),
+      onPressed: _audioPlayer.hasPrevious ? _audioPlayer.seekToPrevious : null,
+    );
+  }
+
+  Widget _playPauseButton(PlayerState? playerState) {
+    if (playerState?.playing != true) {
+      return IconButton(
+        icon: const Icon(Icons.play_arrow),
+        iconSize: 64.0,
+        onPressed: _audioPlayer.play,
+      );
+    } else if (playerState?.processingState != ProcessingState.completed) {
+      return IconButton(
+        icon: const Icon(Icons.pause),
+        iconSize: 64.0,
+        onPressed: _audioPlayer.pause,
+      );
+    } else {
+      return IconButton(
+        icon: const Icon(Icons.play_arrow),
+        iconSize: 64.0,
+        onPressed: () => _audioPlayer.seek(Duration.zero,
+            index: _audioPlayer.effectiveIndices?.first),
+      );
+    }
+  }
+
+  Widget _nextButton() {
+    return IconButton(
+      icon: const Icon(Icons.skip_next),
+      onPressed: _audioPlayer.hasNext ? _audioPlayer.seekToNext : null,
+    );
+  }
+
+  Widget _repeatButton(BuildContext context, LoopMode loopMode) {
+    final icons = [
+      const Icon(Icons.repeat),
+      Icon(Icons.repeat, color: Theme.of(context).colorScheme.secondary),
+      Icon(Icons.repeat_one, color: Theme.of(context).colorScheme.secondary),
+    ];
+    const loopModes = [
+      LoopMode.off,
+      LoopMode.all,
+      LoopMode.one,
+    ];
+    final index = loopModes.indexOf(loopMode);
+    return IconButton(
+      icon: icons[index],
+      onPressed: () {
+        _audioPlayer.setLoopMode(
+            loopModes[(loopModes.indexOf(loopMode) + 1) % loopModes.length]);
+      },
+    );
+  }
+}
