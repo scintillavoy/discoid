e7f3a1cb6fcc801c14987acdfee2d2de3bec1af3
scintillavoy <scintillavoy@gmail.com>
2022-01-30 19:45:04 +0900

    Start playing when autoplay button is clicked

--- lib/widgets/autoplay_button.dart
+++ lib/widgets/autoplay_button.dart
@@ -1,3 +1,5 @@
+import 'package:discoid/models/playlist.dart';
+import 'package:discoid/services/audio_player_service.dart';
 import 'package:discoid/services/media_library_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -12,6 +14,15 @@ class AutoplayButton extends StatelessWidget {
         return ElevatedButton(
           onPressed: () {
             mediaLibraryService.generateAutoplayTracks();
+            final audioPlayerService =
+                Provider.of<AudioPlayerService>(context, listen: false);
+            audioPlayerService.setPlaylist(Playlist(
+              name: 'autoplayTracks',
+              items: mediaLibraryService.autoplayTracks,
+            ));
+            audioPlayerService
+                .seekToIndex(0, reshuffle: true)
+                .then((_) => audioPlayerService.audioPlayer.play());
           },
           child: const Text("Autoplay"),
         );
