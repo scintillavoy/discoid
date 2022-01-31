2e9532baeb27706bb4f25201a04664a188836af5
scintillavoy <scintillavoy@gmail.com>
2022-01-31 16:32:00 +0900

    Make UI responsive

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,57 +7,77 @@ import 'package:discoid/widgets/player_controller.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+  static const int mobileWidth = 850;
+
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            DefaultTabController(
-              length: 4,
-              child: Expanded(
-                child: Scaffold(
-                  appBar: AppBar(
-                    title: const Text('d_d'),
-                    bottom: const TabBar(
-                      isScrollable: true,
-                      tabs: [
-                        Tab(text: "Autoplay"),
-                        Tab(text: "Tracks"),
-                        Tab(text: "Albums"),
-                        Tab(text: "Playlists"),
-                      ],
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: SafeArea(
+            child: Column(
+              children: [
+                DefaultTabController(
+                  length: constraints.maxWidth < mobileWidth ? 5 : 4,
+                  child: Expanded(
+                    child: Scaffold(
+                      appBar: AppBar(
+                        title: const Text('d_d'),
+                        bottom: TabBar(
+                          isScrollable: true,
+                          tabs: [
+                            const Tab(text: "Autoplay"),
+                            const Tab(text: "Tracks"),
+                            const Tab(text: "Albums"),
+                            const Tab(text: "Playlists"),
+                            if (constraints.maxWidth < mobileWidth)
+                              const Tab(text: "Lyrics"),
+                          ],
+                        ),
+                      ),
+                      body: Row(
+                        children: [
+                          if (constraints.maxWidth >= mobileWidth) ...[
+                            const SizedBox(
+                              width: 550,
+                              child: LyricsPanel(),
+                            ),
+                            VerticalDivider(
+                              color: Theme.of(context).colorScheme.primary,
+                              thickness: 1,
+                            ),
+                          ],
+                          Expanded(
+                            child: TabBarView(
+                              children: [
+                                const AutoplayScreen(),
+                                const Tracks(),
+                                const Albums(),
+                                const Playlists(),
+                                if (constraints.maxWidth < mobileWidth)
+                                  Row(
+                                    children: const [
+                                      Expanded(
+                                        child: LyricsPanel(),
+                                      ),
+                                    ],
+                                  ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                  body: Row(
-                    children: [
-                      const SizedBox(
-                        width: 550,
-                        child: LyricsPanel(),
-                      ),
-                      VerticalDivider(
-                        color: Theme.of(context).colorScheme.primary,
-                        thickness: 1,
-                      ),
-                      const Expanded(
-                        child: TabBarView(children: [
-                          AutoplayScreen(),
-                          Tracks(),
-                          Albums(),
-                          Playlists(),
-                        ]),
-                      ),
-                    ],
-                  ),
                 ),
-              ),
+                const PlayerController(),
+              ],
             ),
-            const PlayerController(),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
